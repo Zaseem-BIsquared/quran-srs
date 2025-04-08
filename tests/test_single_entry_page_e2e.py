d317e85a9d7bbff4fb2d2597ed86497b03e1dc1d
Zaseem-BIsquared <zaseem@bisquared.com>
2025-04-08 08:13:03 +0530

    add save_button_navigation test

--- tests/test_single_entry_page_e2e.py
+++ tests/test_single_entry_page_e2e.py
@@ -28,14 +28,17 @@ def test_form_submission(page: Page) -> None:
     page.get_by_role("textbox", name="Revision Date").fill(next_day)
     page.get_by_role("radio", name="😄 Ok").check()
     page.get_by_role("button", name="Save").click()
-    expect(page.get_by_role("heading", name="- S3 Aal-e-Imran")).to_contain_text(
-        "57 - S3 Aal-e-Imran"
-    )
     # Check if the revision was added to the database
     last_revision = revisions()[-1]
     assert last_revision["page"] == 56
     assert last_revision["revision_date"] == next_day
     assert last_revision["rating"] == 0
+
+
+def test_save_button_navigation(page: Page) -> None:
+    page.goto("http://localhost:5001/revision/add?page=56")
+    page.get_by_role("button", name="Save").click()
+    expect(page).to_have_url("http://localhost:5001/revision/add?page=57")
 
 
 def test_cancel_button_navigation(page: Page) -> None:
